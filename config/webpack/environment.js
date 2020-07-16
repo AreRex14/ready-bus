3e31d33c21226e0b7888d080db2968e8169924c4
AreRex14 <arerifxynwa@gmail.com>
2020-07-16 17:35:00 +0800

    setup jquery and jquery-ui integration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jQuery: 'jquery',
+    Rails: '@rails/ujs'
+  }))
+  
+  
+const aliasConfig = {
+    'jquery': 'jquery/src/jquery',
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+};
+
+environment.config.set('resolve.alias', aliasConfig);
+
 module.exports = environment
