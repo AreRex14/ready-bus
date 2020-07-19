9f3219269823229055fc3dedfaeff8f99d8958ad
AreRex14 <arerifxynwa@gmail.com>
2020-07-19 16:10:40 +0800

    Revert "create controller seating, edit index view, set root"
    This reverts commit 114c20cc96b4099ba7f8ecabb45ab848ff63be04.
    
    revert create controller seating index

--- test/controllers/seating_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SeatingControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get seating_index_url
-    assert_response :success
-  end
-
-end
