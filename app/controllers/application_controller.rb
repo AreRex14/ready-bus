822b2a9fac5cd065bcf233208cf96653bee3b4fd
AreRex14 <arerifxynwa@gmail.com>
2020-07-20 01:42:34 +0800

    generate devise views passengers and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :authenticate_passenger!
+
+    def after_sign_in_path_for(resource)
+        new_booking_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :passengers
   resources :bookings
   
-  root to: 'bookings#new'
+  root to: 'welcome#index'
 end
