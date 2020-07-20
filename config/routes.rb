2953310d4568144340a2abe157b7e790042f144f
AreRex14 <arerifxynwa@gmail.com>
2020-07-20 15:42:14 +0800

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :passengers
-  resources :bookings
+  resources :bookings, only: [:index, :show, :new, :create]
+  resources :travelers, only: [:show, :new, :create]
+  resources :payments
   
   root to: 'welcome#index'
 end
