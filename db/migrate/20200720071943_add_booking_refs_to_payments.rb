b8ef4b9c7214ada94989060816bd365bd4068143
AreRex14 <arerifxynwa@gmail.com>
2020-07-21 17:04:00 +0800

    remove payments migrations

--- db/migrate/20200720071943_add_booking_refs_to_payments.rb
+++
@@ -1,5 +0,0 @@
-class AddBookingRefsToPayments < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :payments, :booking, null: true, foreign_key: true
-  end
-end
--- db/migrate/20200720100746_remove_payment_from_booking.rb
+++
@@ -1,5 +0,0 @@
-class RemovePaymentFromBooking < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :bookings, :payment, foreign_key: true
-  end
-end
