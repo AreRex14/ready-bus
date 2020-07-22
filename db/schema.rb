14da211ac339ad0d0224897036275b226cf8ae02
AreRex14 <arerifxynwa@gmail.com>
2020-07-22 16:01:27 +0800

    add passenger references to travelers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_070002) do
+ActiveRecord::Schema.define(version: 2020_07_22_031756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,10 @@ ActiveRecord::Schema.define(version: 2020_07_21_070002) do
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "passenger_id"
+    t.index ["passenger_id"], name: "index_travelers_on_passenger_id"
   end
 
   add_foreign_key "bookings", "travelers"
+  add_foreign_key "travelers", "passengers"
 end
