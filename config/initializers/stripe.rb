42051ca10eacecc62ee0c0ec87be92793770d535
AreRex14 <arerifxynwa@gmail.com>
2020-07-23 16:59:20 +0800

    set to use ENV keys for Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
+Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
+}
+
 Stripe.api_key = Rails.application.credentials.stripe_secret_key
