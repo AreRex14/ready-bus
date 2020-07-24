15f90210b28a2e509e2b45e5fd9ec9bca6e27868
AreRex14 <arerifxynwa@gmail.com>
2020-07-24 14:51:19 +0800

    rename routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'home/index'
-  get 'home/terms'
-  get 'home/privacy'
+  get 'home/terms', as: "/terms"
+  get 'home/privacy', as: "/privacy"
 
   resources :travelers, only: [:show, :new, :create]
   #resources :bookings, only: [:index, :show, :new, :create]
