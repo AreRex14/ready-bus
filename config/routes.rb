cfa78fac0778b50af7e9cc6b3b9e9f05cdc3c46f
AreRex14 <arerifxynwa@gmail.com>
2020-08-06 16:09:46 +0800

    add buses routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
       resources :users
       resources :operators
+      resources :buses
       resources :bookings
       resources :travelers
 
