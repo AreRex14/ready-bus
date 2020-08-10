70e565643852030c9f5c27991545fa72c2604bba
AreRex14 <arerifxynwa@gmail.com>
2020-08-10 12:35:33 +0800

    migrate seat and bus references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_043407) do
+ActiveRecord::Schema.define(version: 2020_08_10_041658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,49 @@ ActiveRecord::Schema.define(version: 2020_07_23_043407) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "traveler_id"
     t.index ["traveler_id"], name: "index_bookings_on_traveler_id"
+  end
+
+  create_table "buses", force: :cascade do |t|
+    t.string "plate_no"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "operator_id", null: false
+    t.index ["operator_id"], name: "index_buses_on_operator_id"
+  end
+
+  create_table "operators", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "phone_no"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string "pickup_station"
+    t.datetime "pickup_time"
+    t.string "dropoff_station"
+    t.datetime "dropoff_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "bus_id", null: false
+    t.index ["bus_id"], name: "index_schedules_on_bus_id"
+  end
+
+  create_table "seats", force: :cascade do |t|
+    t.string "label"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "bus_id", null: false
+    t.index ["bus_id"], name: "index_seats_on_bus_id"
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.string "place"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "travelers", force: :cascade do |t|
@@ -49,10 +92,15 @@ ActiveRecord::Schema.define(version: 2020_07_23_043407) do
     t.string "card_exp_month"
     t.string "card_exp_year"
     t.string "card_last4"
+    t.boolean "admin_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "travelers"
+  add_foreign_key "buses", "operators"
+  add_foreign_key "schedules", "buses"
+  add_foreign_key "seats", "buses"
   add_foreign_key "travelers", "users"
 end
