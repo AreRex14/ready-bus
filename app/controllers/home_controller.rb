c060fa4c00554cdff6f52272205933ece8e04ac6
AreRex14 <arerifxynwa@gmail.com>
2020-08-12 18:13:00 +0800

    add search functionalities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,16 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @schedules = Schedule.all
+  end
+
+  def search
+    @schedules = Schedule.all
+    @search = params["search"]
+    if @search.present?
+      @dropoff_station = @search["dropoff_station"]
+      @schedules = Schedule.where("dropoff_station ILIKE ?", @dropoff_station)
+    end
   end
 
   def terms
