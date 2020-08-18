e88d761d100e8150c9a16ea01a821fcc6283d061
AreRex14 <arerifxynwa@gmail.com>
2020-08-18 17:21:04 +0800

    set payload validations

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,2 +1,26 @@
 class Seat < ApplicationRecord
+    belongs_to :bus
+
+    PAYLOAD_SCHEMA = "#{Rails.root}/app/models/schemas/seat_payload.json"
+    validates :payload, presence: true, json: { message: -> (err) { err }, schema: PAYLOAD_SCHEMA }
+
+    # serialize :payload, JsonbSerializers
+
+    # store_accessor :payload, :ids
+
+    # def ids_id
+    #     self.ids['id']
+    # end
+
+    # def ids_labeled
+    #     self.ids['labeled']
+    # end
+
+    # def ids_booked
+    #     self.ids['booked']
+    # end
+
+    # def ids_booked=(value)
+    #     self.ids['booked'] = value
+    # end
 end
